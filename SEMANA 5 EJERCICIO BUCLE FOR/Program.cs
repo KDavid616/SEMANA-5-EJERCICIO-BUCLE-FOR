cb53644b07c6053eb5f7b71580f18dc6f485e135
KDavid616 <124007266+KDavid616@users.noreply.github.com>
2023-02-25 10:57:57 -0600

    Add files via upload

---
+++ SEMANA 5 EJERCICIO BUCLE FOR/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace RangoPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorInicial;
+            int valorFinal;
+
+            Console.WriteLine("digite el valor inicial:");
+            valorInicial = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el valor final:");
+            valorFinal = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Los números pares son: ");
+
+            for (int i = valorInicial; i <= valorFinal; i += 2)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
